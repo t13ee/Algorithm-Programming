8a10ebe748264a257e46e2e132547aaff98d2b09
t13ee <t13ee2005@gmail.com>
2023-12-19 17:14:56 +0700

    codelearn.io

---
+++ codelearn.io/Training/C/GroupedBits.c
@@ -0,0 +1,33 @@
+// #include <stdio.h>
+#include <stdbool.h>
+
+
+int groupedBits(int n)
+{
+    int count=0, b;
+    bool accept=true;
+
+    while ((float)n / 2 != 0)
+    {
+        b = n % 2;
+        n /= 2;
+
+        if (b == 1 && accept)
+        {
+            count += 1;
+            accept = false;
+        } else if (b == 0)
+            accept = true; 
+    }
+    
+    return count;
+}
+
+/*
+int main()
+{
+    printf("%d", groupedBits(1529));
+    return 0;
+}
+*/
+
