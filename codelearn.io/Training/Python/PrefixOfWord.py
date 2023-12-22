2bcf6ea005d5be927c45104eef85f2f68cb8989e
t13ee <t13ee2005@gmail.com>
2023-12-22 19:53:40 +0700

    'codelearn.io'

--- codelearn.io/Training/Python/PrefixOfWord.py
+++
@@ -1,6 +0,0 @@
-def prefix_of_word(s, p):
-    for num, t in enumerate(s.split(" ")):
-        if t.startswith(p):
-            return num + 1
-    
-    return -1
--- codelearn.io/Training/Python/SumOfThree.py
+++
@@ -1,4 +0,0 @@
-def sum_of_three(s):
-    if float(s) % 3 == 0:
-        return True
-    return False
