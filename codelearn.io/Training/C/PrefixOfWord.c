28b0a5462a6907e5d220f735185df2ae1a757aa6
t13ee <t13ee2005@gmail.com>
2023-12-22 18:32:57 +0700

    'codelearn.io'

--- codelearn.io/Training/C/PrefixOfWord.c
+++ codelearn.io/Training/C/PrefixOfWord.c
@@ -7,6 +7,7 @@ bool startswith(const char *pre, const char *str)
     size_t lenpre = strlen(pre),
            lenstr = strlen(str);
     return lenstr < lenpre ? false : memcmp(pre, str, lenpre) == 0;
+    
 }
 
 
